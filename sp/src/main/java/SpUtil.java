9a3eee0e85897fd5d38734d157e695d623688575
Pinepond <81852649+Pinepond@users.noreply.github.com>
2023-07-17 09:35:23 +0900

    Update SpUtil.java

--- sp/src/main/java/SpUtil.java
+++ sp/src/main/java/SpUtil.java
@@ -8,11 +8,18 @@ import java.net.URL;
 import java.net.URLClassLoader;
 import java.net.URLStreamHandler;
 import java.nio.file.Files;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
+
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.api.ContentResponse;
+import org.eclipse.jetty.client.util.StringContentProvider;
+import org.eclipse.jetty.http.HttpMethod;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -20,21 +27,14 @@ import com.google.gson.JsonSyntaxException;
 
 public class SpUtil {
 
-	// == Scanner sample ==
-//	Scanner scan = new Scanner(System.in);
-//	while (true) {
-//	    String nextLine = scan.nextLine();
-//		
-//	}
-
-	// == thread sample ==
+	// thread sample
 	// ExecutorService service = Executors.newFixedThreadPool(300);
 	// service.execute(() -> {
-        // });
+	// });
 	// service.submit(() -> {
-	//   return something;
-        // });
-	
+	// return something;
+	// });
+
 	// file
 	public static String fileToString(String path) throws IOException {
 		return Files.lines(new File(path).toPath()).collect(Collectors.joining(System.lineSeparator()));
@@ -43,12 +43,42 @@ public class SpUtil {
 	public static List<String> fileToStrings(String path) throws IOException {
 		return Files.lines(new File(path).toPath()).collect(Collectors.toList());
 	}
-	
+
 	public static void writeStringToFile(String path, String data) throws IOException {
 		Files.write(new File(path).toPath(), data.getBytes());
 	}
-	
-	//json
+
+	// send http request
+	public static ContentResponse sendHttpPostWithContent(String contentJson, String uri) {
+		HttpClient httpClient = new HttpClient();
+		ContentResponse contentRes = null;
+		try {
+			httpClient.start();
+			contentRes = httpClient.newRequest(uri).method(HttpMethod.POST)
+					.content(new StringContentProvider(contentJson)).send();
+			httpClient.stop();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return contentRes;
+	}
+
+	public static ContentResponse sendHttpGet(String uri) {
+		ContentResponse contentRes = null;
+		HttpClient httpClient = new HttpClient();
+		try {
+			httpClient.start();
+			contentRes = httpClient.newRequest(uri).method(HttpMethod.GET).send();
+			System.out.println("contentRes.getContentAsString() : " + contentRes.getContentAsString());
+			httpClient.stop();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return contentRes;
+	}
+
+	// json
 	public static String objectToJsonString(Object obj) throws JsonSyntaxException, IOException {
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		;
@@ -62,10 +92,11 @@ public class SpUtil {
 
 	public static <T> T jsonFileToObject(String path, Class<T> targetClazz) throws JsonSyntaxException, IOException {
 		Gson gson = new Gson();
-		//Gson gson = new GsonBuilder().setFieldNamingStrategy(f -> f.getName().toUpperCase()).create();
+		// Gson gson = new GsonBuilder().setFieldNamingStrategy(f ->
+		// f.getName().toUpperCase()).create();
 		return gson.fromJson(fileToString(path), targetClazz);
 	}
-	
+
 	// String
 	public static boolean isNullOrEmpty(String str) {
 		if (str == null || str.length() == 0) {
@@ -73,7 +104,7 @@ public class SpUtil {
 		}
 		return false;
 	}
-	
+
 	// sleep
 	public static void sleep(long ms) {
 		try {
@@ -83,13 +114,12 @@ public class SpUtil {
 			e.printStackTrace();
 		}
 	}
-	
+
 	// random uuid
 	public static String getRandomUUID() {
 		return UUID.randomUUID().toString();
 	}
-	
-	
+
 	// class loading
 
 	public static Class<?> loadClassFile(String classFilePath, String classNameWithPackgePath) throws Exception {
@@ -102,7 +132,6 @@ public class SpUtil {
 
 		urls.add(new URL(null, "file:" + classPathFile.getCanonicalPath() + File.separator, urlStHandler));
 
-
 		URLClassLoader urlLoader = new URLClassLoader((URL[]) urls.toArray(new URL[urls.size()]));
 
 		return urlLoader.loadClass(classNameWithPackgePath);
@@ -114,8 +143,7 @@ public class SpUtil {
 			return c.getName() == methodName;
 		}).findFirst().get();
 	}
-	
-	
+
 	// execute new process
 	public static void execProc(String batFileName, String... params) {
 		String path = System.getProperty("user.dir");
@@ -138,5 +166,39 @@ public class SpUtil {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		}
+	}
+
+	// encrypt sha-256
+	public static String encryptSHA256(String text) {
+		MessageDigest md = null;
+		try {
+			md = MessageDigest.getInstance("SHA-256");
+		} catch (NoSuchAlgorithmException e) {
+		}
+		md.update(text.getBytes());
+
+		return bytesToHex(md.digest());
+	}
+
+	private static String bytesToHex(byte[] bytes) {
+		StringBuilder builder = new StringBuilder();
+		for (byte b : bytes) {
+			builder.append(String.format("%02x", b));
+		}
+		return builder.toString();
+	}
+	
+	// get time diff
+	public static long timeDiffSecs(long beforeTime, long afterTime) {
+		if(afterTime == 0) {
+			afterTime = System.currentTimeMillis();
+		}
+		return (afterTime - beforeTime) / 1000;
+	}
+	
+	public static boolean isTimeDelayedOverSec(long beforeTime, long delayedSec) {
+		long afterTime = System.currentTimeMillis();		
+		long calc = (afterTime - beforeTime) / 1000;
+		return calc > delayedSec ? true : false;
 	}
 }
